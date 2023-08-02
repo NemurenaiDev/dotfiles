636a535122ed9d4b6643576301c6b98ec20860fb
SilvanVL <vlad95700@gmail.com>
2023-08-03 01:50:20 +0300

    update

--- shell-extension.sh
+++ shell-extension.sh
@@ -1,15 +1,13 @@
 DOTFILES_PATH=~/Hyprland
 
+
 lfcd() {
-        LF_SHELLCD_TEMPDIR="$(mktemp -d -t lf-shellcd-XXXXXX)"
-        export LF_SHELLCD_TEMPDIR
-        lf -last-dir-path "$LF_SHELLCD_TEMPDIR/lastdir" -command "source '$HOME/.config/lf/lfrc'" "$@"
-        if [ -e "$LF_SHELLCD_TEMPDIR/changecwd" ] && \
-                dir="$(cat "$LF_SHELLCD_TEMPDIR/lastdir")" 2>/dev/null; then
-                cd "$dir"
-        fi
-        rm -rf "$LF_SHELLCD_TEMPDIR"
-        unset LF_SHELLCD_TEMPDIR    
+		rm "/tmp/lf-shellcd-lastdir" "/tmp/lf-shellcd-changecwd" 2>/dev/null
+    lf -last-dir-path "/tmp/lf-shellcd-lastdir" -command "source '$HOME/.config/lf/lfrc'" "$@"
+    if [ -e "/tmp/lf-shellcd-changecwd" ] && \
+				dir="$(cat "/tmp/lf-shellcd-lastdir")" 2>/dev/null; then
+				cd "$dir"
+    fi
 }
 
 
