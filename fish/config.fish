84d9f29d9ec6f4805ed81d8797c8c3e3d8b9adc0
SilvanVL <vlad95700@gmail.com>
2023-12-20 23:37:38 +0200

    Keybinds fix + filesystem navigation

--- fish/config.fish
+++ fish/config.fish
@@ -35,6 +35,20 @@ if status is-interactive
     alias dc-start 'dc down && dc build && dc up -d && clear && dc up'
 
 
+    alias f "wl-copy (find -L . -type f -not -path '*/.*' -print | fzf)"
+    alias g "cd (find -L . -type d -not -path '*/.*' -print | fzf)"
+
+    alias gr 'cd /'
+    alias gm 'cd /mnt'
+    alias ge 'cd /etc'
+    alias go 'cd /opt'
+    alias gu 'cd /usr'
+
+    alias gh 'cd ~'
+    alias gc 'cd ~/.config'
+    alias gd 'cd ~/Downloads'
+    alias gp 'cd ~/Projects'
+
 #     function fish_greeting
 #         echo "$(pfetch)"
 #     end
