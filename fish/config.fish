c024c5ca85526d5e7107bda3df7cb24a5e91681e
SilvanVL <vlad95700@gmail.com>
2023-12-27 12:48:29 +0200

    Archive lf & thunar + use nautilus + add dconf db + advanced navigation (lastcd, histcd)

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,19 @@
 if status is-interactive
     set -xg DOTFILES_PATH ~/Hyprland
+    set -xg VARIABLES_PATH ~/Hyprland/scripts/variables
     set -xg VISUAL nvim
     set -xg EDITOR nvim
 
     source "$DOTFILES_PATH/fish/setenv.fish"
 
-    if test (tty) = "/dev/tty1"
-        if not pgrep -x "Hyprland" >/dev/null
+    if test (tty) = '/dev/tty1'
+        if not pgrep -x 'Hyprland' >/dev/null
             Hyprland
         end
     end
 
 
     starship init fish | source
-
-    alias lf='cd $(~/.config/hypr/scripts/lfcd)'
 
     alias ssh 'kitten ssh'
     alias ls 'ls --color=auto'
@@ -35,9 +34,6 @@ if status is-interactive
     alias dc-start 'dc down && dc build && dc up -d && clear && dc up'
 
 
-    alias f "wl-copy (fd --type f --follow -E node_modules -E vendor -E /proc -E /run -E /srv -E /sys -E /lib -E /lib64 -E /sbin -E /bin -E /mnt | fzf -e)"
-    alias g "cd (fd --type d --follow -E node_modules -E vendor -E /proc -E /run -E /srv -E /sys -E /lib -E /lib64 -E /sbin -E /bin -E /mnt | fzf -e)"
-
     alias gr 'cd /'
     alias gm 'cd /mnt'
     alias ge 'cd /etc'
@@ -48,7 +44,19 @@ if status is-interactive
     alias gc 'cd ~/.config'
     alias gd 'cd ~/Downloads'
     alias gp 'cd ~/Projects'
+    
 
+    alias ff 'wl-copy (fd --type f --follow -E node_modules -E vendor -E /proc -E /run -E /srv -E /sys -E /lib -E /lib64 -E /sbin -E /bin -E /mnt | fzf -e)'
+    alias gg 'cd (fd --type d --follow -E node_modules -E vendor -E /proc -E /run -E /srv -E /sys -E /lib -E /lib64 -E /sbin -E /bin -E /mnt | fzf -e)'
+    alias ggg 'cd (cat $VARIABLES_PATH/histcd | fzf -e)'
+    alias g "cdoriginal '$(cat $VARIABLES_PATH/lastcd)'"
+
+    functions -c cd cdoriginal
+    function cd
+        cdoriginal $argv
+        echo "$(pwd)" > "$VARIABLES_PATH/lastcd"
+        echo "$(pwd)" >> "$VARIABLES_PATH/histcd"
+    end
 
     functions --erase fish_greeting
 end
