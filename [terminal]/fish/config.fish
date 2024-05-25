cb1b31abc0dbe1db1fe2d615bacd1710e319f061
Nemurenai <vlad95700@gmail.com>
2024-05-25 12:56:31 +0300

    Copyq & lots of minor changes

--- [terminal]/fish/config.fish
+++ [terminal]/fish/config.fish
@@ -15,35 +15,36 @@ if status is-interactive
     starship init fish | source
 
 
-    alias ssh 'kitten ssh'
     alias ls 'ls --color=auto'
     alias grep 'grep --color=auto'
     alias diff 'diff --color=auto'
     alias ip 'ip --color=auto'
+    alias ssh 'kitten ssh'
+    alias rm 'trash -v'
 
     alias vim 'nvim'
     alias vi 'nvim'
+
+    alias jetbra 'notify-send "Jetbra command ran" && sudo ~/.config/dotfiles/scripts/jetbra'
 
     alias l 'lsd --group-dirs first'
     alias ll 'lsd -lh --group-dirs first'
     alias lll 'lsd -lAh --group-dirs first'
 
-
     alias gr 'cd /'
-    alias gm 'cd /mnt'
-    alias ge 'cd /etc'
-    alias go 'cd /opt'
-    alias gu 'cd /usr'
-
-    alias gh 'cd ~'
     alias gc 'cd ~/.config'
     alias gd 'cd ~/Downloads'
     alias gp 'cd ~/Projects'
 
-
-    alias gg 'cd (fd --type d --follow -E node_modules -E vendor -E /proc -E /run -E /srv -E /sys -E /lib -E /lib64 -E /sbin -E /bin -E /mnt | fzf -e)'
-    alias ggg 'cd (cat $VARIABLES_PATH/histcd | sort | uniq | fzf -e)'
     alias g 'cd-original "$(cat $VARIABLES_PATH/lastcd)"'
+    alias gg 'cd (fd --type d --follow -E node_modules -E vendor -E /proc -E /run -E /srv -E /sys -E /lib -E /lib64 -E /sbin -E /bin -E /mnt | fzf --exact)'
+
+    function ggg
+        set -l selected_path (cat $VARIABLES_PATH/histcd | sort -u | while read -la line; if test -d "$line"; echo "$line"; end; end | fzf --exact)
+        if test -n "$selected_path"
+            cd "$selected_path"
+        end
+    end
 
     functions -c cd cd-original
     function cd
@@ -54,26 +55,22 @@ if status is-interactive
 
     function pc
         switch $argv[1]
-            case "i"
+            case "i" "install"
                 pikaur -S --nodiff --noedit $argv[2..-1]
-            case "install"
-                pikaur -S --nodiff --noedit $argv[2..-1]
-            case "remove"
+            case "remove" "r"
                 pikaur -Rns $argv[2..-1]
-            case "update"
-                sudo pikaur -Syu --nodiff --noedit
-            case "search"
+            case "update" "u"
+                sudo pikaur -Syu --nodiff --noedit && jetbra
+            case "search" "s"
                 pikaur -Ss $argv[2..-1]
-            case "list"
-                pikaur -Q | grep "$(string join '\|' $argv[2..-1])"
-            case "ls"
-                pikaur -Q | grep "$(string join '\|' $argv[2..-1])"
-            case "info"
+            case "list" "ls"
+                pikaur -Q | grep (string join \| $argv[2..-1])
+            case "info" "i"
                 pikaur -Qi $argv[2..-1]
             case "clear"
                 pikaur -Scc
             case "*"
-                echo "Usage: pc [ i | install | remove | update | search | list | ls | info | clear] [packages...]"
+                echo "Usage: pc [(i|install) | (r|remove) | (u|update) | (s|search) | (ls|list) | info | clear] [packages...]"
         end
     end
 
